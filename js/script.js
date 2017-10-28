b53ecb6483227f50ea3b53c3116d875c439c7663
kaiaz <azrailkai@gmail.com>
2017-10-28 19:30:46 +0600

    Add keyListener function contain eventlistener for keydown event, add change direction function

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ Snake.prototype.init = function () {
     this.initField();
     this.initSnake();
 
-    this.initMoving();
+    this.makeStep();
+    this.listenPress();
 }
 
 // init Snake - field: cellSize = 20px, making 40 cells on y axis and 40 cells x axis
@@ -32,13 +33,116 @@ Snake.prototype.initSnake = function () {
             snakeItem.style.width = this.options.cellSize + 'px';
         snakeItem.style.top = 0;
         snakeItem.style.left = (i * this.options.cellSize) + 'px';
-        this.snake.push(snakeItem);
+
+        this.snake.push({
+            element: snakeItem,
+            posX: i,
+            posY: 0,
+            direction: 'right'
+        });
         this.element.appendChild(snakeItem);
     }
 }
+// this function make snake array moving on field. Loop fore iterating while index i < snake length
+// var snake keep every index (cell) snake array and  make it change position on field
+// call this function in other function - makeStep, witch keep inside setTimeMount function
 
-Snake.prototype.initMoving = function () {
-    
+Snake.prototype.moveSnake = function () {
+    for(var i = 0, len = this.snake.length; i < len; i++) {
+        var snakeItem = this.snake[i];
+        switch(snakeItem.direction){
+            case 'left':
+                snakeItem.posX--;
+                snakeItem.element.style.left = (snakeItem.posX * this.options.cellSize) + 'px';
+                break;
+            case 'right':
+                snakeItem.posX++;
+                snakeItem.element.style.left = (snakeItem.posX * this.options.cellSize) + 'px';
+                break;
+            case  'top':
+                snakeItem.posY--;
+                snakeItem.element.style.top = (snakeItem.posY * this.options.cellSize) + 'px';
+                break;
+            case  'bottom':
+                snakeItem.posY++;
+                snakeItem.element.style.top = (snakeItem.posY * this.options.cellSize) + 'px';
+                break;
+        }
+    }
+
+    // this loop change direction steel cell snake
+
+    for (var i = 0; i < len - 1; i++) {
+        this.snake[i].direction = this.snake[i + 1].direction;
+    }
+}
+
+//Assign direction head snake received in listenPress function
+
+Snake.prototype.changeDirection = function (direction) {
+    var head = this.snake[this.snake.length - 1];
+    head.direction = direction;
+}
+
+
+// listen key event if press one of thees key calling changeDirection function with argument contain direction match key
+
+Snake.prototype.listenPress = function () {
+    var _this = this;
+    window.addEventListener('keydown', function () {
+        var e = window.event;
+        switch (e.keyCode) {
+            case 87:
+            case 38:
+                _this.changeDirection('top');
+                break;
+            case 83:
+            case 40:
+                _this.changeDirection('bottom');
+                break;
+            case 65:
+            case 37:
+                _this.changeDirection('left');
+                break;
+            case 68:
+            case 39:
+                _this.changeDirection('right');
+                break;
+        }
+        console.log(e.keyCode);
+    })
+}
+
+// Check position snake head - last element in array snake
+
+Snake.prototype.checkGameOver = function () {
+    var head = this.snake[this.snake.length - 1];
+
+    if(head.posX === this.options.field.sizeX) {
+        return true;
+    } else if(head.posY === this.options.field.sizeY) {
+        return true;
+    } else if (head.posX < 0) {
+        return true;
+    } else if (head.posY < 0) {
+        return true;
+    }
+
+    return false
+}
+
+// keep inside setTimeout  function. This call makeStep and itself inside makeStep
+// makeStep checks result returned in function CheckGameOver if !checkGameOver this working
+Snake.prototype.makeStep = function () {
+    var _this = this;
+    if(!this.checkGameOver()) {
+        this.moveSnake();
+       return setTimeout(function () {
+            _this.makeStep();
+        }, this.options.snake.speed)
+    }
+
+    alert('Game over!')
 }
 
 var createSnake = new Snake({
@@ -49,7 +153,8 @@ var createSnake = new Snake({
         sizeY: 40
     },
     snake: {
-        minSize: 2,
+        minSize: 4,
+        speed: 500,
         defaultDirection: 'right',
         posX: 0,
         posY: 0
