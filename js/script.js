55229da67ee968d5fb202bf31d6b2bd3f402f377
kaiaz <azrailkai@gmail.com>
2017-10-28 13:40:59 +0600

    Describe initSnake method used the loop for create and append new div with class snake_item in snake div

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ function Snake(options) {
     this.options = options;
     this.element = this.options.element,
     this.snake = [];
-
+    this.food = [];
+    this.gameOver = false;
     this.init()
 }
 
@@ -11,6 +12,8 @@ function Snake(options) {
 Snake.prototype.init = function () {
     this.initField();
     this.initSnake();
+
+    this.initMoving();
 }
 
 // init Snake - field: cellSize = 20px, making 40 cells on y axis and 40 cells x axis
@@ -19,8 +22,23 @@ Snake.prototype.initField = function () {
     this.element.style.height = (this.options.field.sizeY * this.options.cellSize) + 'px';
 }
 
+// init snake method - use loop for take property snake min size and assign in len var, until then len > i
+// loop create div with class snake_item and push in massive after appending in snake every new div use the loop
 Snake.prototype.initSnake = function () {
+    for(var i = 0, len = this.options.snake.minSize; i < len; i++) {
+        var snakeItem = document.createElement('div');
+        snakeItem.className = 'snake_item';
+        snakeItem.style.height =
+            snakeItem.style.width = this.options.cellSize + 'px';
+        snakeItem.style.top = 0;
+        snakeItem.style.left = (i * this.options.cellSize) + 'px';
+        this.snake.push(snakeItem);
+        this.element.appendChild(snakeItem);
+    }
+}
 
+Snake.prototype.initMoving = function () {
+    
 }
 
 var createSnake = new Snake({
